9e196809e3d2ac9c999c785f5b9e8687b0ecf90c
ChongJiaMing-lab <1221202126@student.mmu.edu.my>
2024-04-01 23:30:41 +0800

    a

--- fyp-jm/admin_cust.php
+++ fyp-jm/admin_cust.php
@@ -34,38 +34,60 @@ tbody td
         <h1 style="margin: 12px 0 0 30px;">User List</Category:Components></h1><hr>
     </div>
     <hr>
-    <input type="text" class="input" placeholder="Search with name">
-    <hr>
-    <p><b>Showing 3 results.</b></p>
+    <form method="POST" action="">
+        <input type="text" class="input" placeholder="Search with name" name="search">
+        <hr>
+    </form>
+    <?php
+          if (isset($_POST["search"])) 
+          {
+                $search = mysqli_real_escape_string($connect, $_POST["search"]);
+                $q = "SELECT * FROM user WHERE name LIKE '%$search%'";
+          } 
+          else 
+          {
+              $q = "SELECT * FROM user";
+          }
+          $result = mysqli_query($connect, $q);
+          $count = mysqli_num_rows($result);
+    ?>
+    <p><b>Showing <?php echo $count ?> results.</b></p>
     <table class="table table-bordered table-hover">
         <thead>
             <tr>
             <th scope="col">#</th>
             <th scope="col">Name</th>
             <th scope="col">Contact</th>
-            <th scope="col"></th>
+            <th scope="col">Age</th>
             </tr>
         </thead>
         <tbody>
-            <tr onclick="window.location='cust_detail';">
-            <th scope="row">1</th>
-            <td>Jiaming<br>
-                <div style="font-size:11px;">>Singapore</div></td>
-            <td style="vertical-align: middle;">Otto</td>
-            <td>@mdo<br>hahaha</td>
-            </tr>
-            <tr>
-            <th scope="row">2</th>
-            <td>Jacob<br>Malaysia</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            </tr>
-            <tr>
-            <th scope="row">3</th>
-            <td>Jacob<br>Malaysia</td>
-            <td>Thornton</td>
-            <td>@twitter</td>
-            </tr>
+        <?php
+          if(mysqli_num_rows($result) > 0)
+          {
+            while($row = mysqli_fetch_array($result))
+            {
+            ?>
+                <tr onclick="window.location='cust_detail.php?user_id=<?php echo $row['user_id'] ?>';">
+                    <th scope="row"><?php echo $row["user_id"] ?></th>
+                    <td><?php echo $row["name"];?><br>
+                        <div style="font-size:11px;"><i>from </i><?php echo $row["country"]?></div></td>
+                    </td>
+                    <td style="vertical-align: middle;">
+                        Telephone.No: <?php echo $row["tel"]?><br>
+                        Email: <?php echo $row["eml"]?>
+                    </td>
+                    <td><?php echo $row["age"]?></td>
+                </tr>
+                
+        <?php
+            }
+          }
+          else
+          {
+            echo"No records found :(";
+          }
+          ?>
         </tbody>
         </table>
 </div><!-- end of main-->
