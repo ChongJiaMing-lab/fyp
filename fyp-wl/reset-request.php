2b8cf83e443a3113f64a3293d69958e73c215e6f
Lam Wei Leong <1221201835@mmu.edu.my>
2024-04-05 21:47:11 +0800

    ..

--- fyp-wl/reset-request.php
+++ fyp-wl/reset-request.php
@@ -6,7 +6,8 @@ if(isset($_POST["reset-request-submit"]))
     $selector = bin2hex(openssl_random_pseudo_bytes(8));
     $token = openssl_random_pseudo_bytes(32);
 
-    $url = ".../create-new-password.php?selector=" .$selector . "$validator=" . bin2hex($token);
+    $url = "http://localhost/fyp-1/fyp-wl/create-new-password.php?selector=" .$selector . "&validator=" . bin2hex($token);
+   
 
     $expires = date("U") +1800;
 
@@ -15,7 +16,7 @@ if(isset($_POST["reset-request-submit"]))
     $userEmail =$_POST["email"];
 
     $sql="DELETE FROM pwdReset WHERE pwdResetEmail=?";
-    $stmt =mysqli_stmt_init($conn);
+    $stmt =mysqli_stmt_init($connect);
     if(!mysqli_stmt_prepare($stmt,$sql))
     {
         echo"There was an error!";
@@ -27,24 +28,24 @@ if(isset($_POST["reset-request-submit"]))
         mysqli_stmt_execute($stmt);
     }
     
-    $sql = "INSERT INTO pwdReset (pwdResetEmail,pwdResetSelector,pwdResetToken,pwdResetExpires) VALUES(?,?,?,?,);";
-    $stmt =mysqli_stmt_init($conn);
+    $sql = "INSERT INTO pwdReset (pwdResetEmail,pwdResetSelector,pwdResetToken,pwdResetExpires) VALUES(?,?,?,?);";
+    $stmt =mysqli_stmt_init($connect);
     if(!mysqli_stmt_prepare($stmt,$sql))
     {
-        echo"There was an error!";
+        echo"There was an error!!!";
         exit();
     }
     else
     {
         $hashedToken = password_hash($token,PASSWORD_DEFAULT);
-        mysqli_stmt_bind_param($stmt, "s",$userEmail, $selector,$hashedToken, $expires );
+        mysqli_stmt_bind_param($stmt, "ssss",$userEmail, $selector,$hashedToken, $expires );
         mysqli_stmt_execute($stmt);
     }
 
     mysqli_stmt_close($stmt);
-    mysqli_close();
+    mysqli_close($connect);
 
-    $to= $userEmail;
+    $to = $userEmail;
 
     $subject = 'Reset your password for skt';
 
@@ -58,13 +59,13 @@ if(isset($_POST["reset-request-submit"]))
     $headers .= "Reply-To:useSKT@gmhtmlail.com\r\n";
     $headers .= "Content-type: text/html\r\n";
 
-    mail($to, $subject,$message,$headers);
+    mail($to, $subject , $message, $headers );
 
-    header("Location: ../forgot_password.php?reset=success");
+    header("Location: forgot_password.php?reset=success");
 }
 else
 {
-    header("Location:../index.php");
+    header("Location:Login.php");
 }
 
 
