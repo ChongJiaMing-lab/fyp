400899a6c5748ff9e69911e7bb4807c43981aeae
Lam Wei Leong <1221201835@mmu.edu.my>
2024-04-27 15:53:23 +0800

    iorder

--- fyp-combine/user/order_detail.php
+++ fyp-combine/user/order_detail.php
@@ -1,5 +1,4 @@
-<!-- ###AIO### -->
-<!-- ###AIO### -->
+
 <!DOCTYPE html>
 <html dir="ltr" lang="en-US" class="ready">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -99,12 +98,20 @@
     #account-order-list .order-list .tab-content>div.is-active {
         display: block;
     }
+   
 </style>
 
 <body class="body-style wide  clamp-1">
     <?php include "head.php" ?>
     <section id="account-order-list" class="section container myaccounts">
+<style>
+ .tab-content
+    {
+        border: 1px black solid;
+        padding-top: 10px;
 
+    }
+</style>
 
 
         <?php
@@ -156,26 +163,14 @@
             <div id="main-content" class="order">
                 <!-- ORDER -->
                 <div class="holder">
-                    <div class="title">My orders</div>
-
-                    <div class="order-list">
-                        <div class="tabs">
-                            <ul>
-                                <li data-tab="tab-to-pay"><a>Processing</a></li>
-                                <li data-tab="tab-to-ship"><a>Shiping</a></li>
-                                <li data-tab="tab-completed"><a>Completed</a></li>
-                                <li data-tab="tab-cancelled"><a>Cancelled</a></li>
-                            </ul>
-                        </div>
-
-                        <div class="tab-content">
+                    
                             
                             
 
                                 <?php
-                               $order_datetime = isset($_GET['order_datetime']) ? urldecode($_GET['order_datetime']) : '';
-                               $user_id = isset($_GET['ID']) ? $_GET['ID'] : '';
-                               
+                               $order_id = $_GET['order_id'];
+                               $user_id = $_GET['ID'];
+                              
                              
                                if ($_SESSION['ID'] !== $user_id) {
                                    die('Error');
@@ -184,11 +179,19 @@
                                $query = "SELECT * FROM cart_order_detail 
                                            INNER JOIN cart ON cart_order_detail.cart_id = cart.cart_id 
                                            INNER JOIN product ON cart.product_id = product.product_id 
-                                           WHERE cart_order_detail.user_id = '$user_id' AND cart_order_detail.order_datetime = '$order_datetime' AND order_status = 'processing'";
+                                           WHERE cart.user_id = '$user_id' AND cart_order_detail.order_id = '$order_id' ";
                                
                                $result = mysqli_query($connect, $query);
 
-                                if ($result) {
+                                if ($result) { ?>
+                                <div class="title">Order #<?php echo $order_id?></div>
+                        <div>
+
+                        </div>
+                    
+
+                        <div class="tab-content">
+                            <?php
                                     while ($row = mysqli_fetch_array($result)) {
                                         ?>
                                        
@@ -233,122 +236,7 @@
                                 ?>
 
                            
-<?php
-                                 $order_datetime = isset($_GET['order_datetime']) ? urldecode($_GET['order_datetime']) : '';
-                                 $user_id = isset($_GET['ID']) ? $_GET['ID'] : '';
-                                 
-                               
-                                 if ($_SESSION['ID'] !== $user_id) {
-                                     die('Error');
-                                 }
-                                 
-                                 $query = "SELECT * FROM cart_order_detail 
-                                             INNER JOIN cart ON cart_order_detail.cart_id = cart.cart_id 
-                                             INNER JOIN product ON cart.product_id = product.product_id 
-                                             WHERE cart_order_detail.user_id = '$user_id' AND cart_order_detail.order_datetime = '$order_datetime' AND order_status = 'shipping'";
-                                 
-                                 $result = mysqli_query($connect, $query);
 
-                                if ($result) {
-                                    while ($row = mysqli_fetch_array($result)) {
-                                        ?>
-                                        <div class="loading"></div>
-                                        <div data-content="tab-to-ship" class="is-active">
-                                        <div class="columns is-mobile">
-                                            <!-- IMG -->
-                                            <div class="column is-2 product-image">
-                                                <a href=".."><img class="img-responsive"
-                                                        src="../image/<?php echo $row['image'] ?>"></a>
-                                            </div>
-
-                                            <!-- INFO -->
-                                            <div class="column is-10">
-                                                <div class="columns is-mobile">
-                                                    <div class="column is-5 product-info" style="padding:50px">
-                                                        <!-- PRODUCT NAME -->
-                                                        <a class="product-name" href=""><?php echo $row['product_name'] ?></a>
-                                                        <!-- VARIATIONS -->
-                                                    </div>
-
-                                                    <!-- UNIT PRICE -->
-                                                    <div class="column is-3 product-price" style="padding:50px">
-                                                        RM<?php echo $row['price'] ?></div>
-
-                                                    <!-- QUANTITY -->
-                                                    <div class="column is-4 product-quantity" style="padding:50px">
-                                                        <div class="input-group">
-                                                            <a class="product-name" href="">QTY: <?php echo $row['qty'] ?></a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        </div>
-                                        <?php
-                                    }
-                                
-                                }
-                                
-                                
-                                ?>
-                           <?php
-                                 $order_datetime = isset($_GET['order_datetime']) ? urldecode($_GET['order_datetime']) : '';
-                                 $user_id = isset($_GET['ID']) ? $_GET['ID'] : '';
-                                 
-                               
-                                 if ($_SESSION['ID'] !== $user_id) {
-                                     die('Error');
-                                 }
-                                 
-                                 $query = "SELECT * FROM cart_order_detail 
-                                             INNER JOIN cart ON cart_order_detail.cart_id = cart.cart_id 
-                                             INNER JOIN product ON cart.product_id = product.product_id 
-                                             WHERE cart_order_detail.user_id = '$user_id' AND cart_order_detail.order_datetime = '$order_datetime' AND order_status = 'completed'";
-                                 
-                                 $result = mysqli_query($connect, $query);
-
-                                if ($result) {
-                                    while ($row = mysqli_fetch_array($result)) {
-                                        ?>
-                                        <div class="loading"></div>
-                                        <div data-content="tab-completed" class="is-active" >
-                                        <div class="columns is-mobile">
-                                            <!-- IMG -->
-                                            <div class="column is-2 product-image">
-                                                <a href=".."><img class="img-responsive"
-                                                        src="../image/<?php echo $row['image'] ?>"></a>
-                                            </div>
-
-                                            <!-- INFO -->
-                                            <div class="column is-10">
-                                                <div class="columns is-mobile">
-                                                    <div class="column is-5 product-info" style="padding:50px">
-                                                        <!-- PRODUCT NAME -->
-                                                        <a class="product-name" href=""><?php echo $row['product_name'] ?></a>
-                                                        <!-- VARIATIONS -->
-                                                    </div>
-
-                                                    <!-- UNIT PRICE -->
-                                                    <div class="column is-3 product-price" style="padding:50px">
-                                                        RM<?php echo $row['price'] ?></div>
-
-                                                    <!-- QUANTITY -->
-                                                    <div class="column is-4 product-quantity" style="padding:50px">
-                                                        <div class="input-group">
-                                                            <a class="product-name" href="">QTY: <?php echo $row['qty'] ?></a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        </div>
-                                        <?php
-                                    }
-                                
-                                }
-                                
-                                
-                                ?>
                             <div data-content="tab-cancelled">
                                 <div class="empty-product-listing">
 
@@ -357,42 +245,12 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    
                 </div>
             </div>
 
         </div>
-        <script>
-            var currenttab = '';
-
-            $(document).on("click", ".order-list .pagination a", function () {
-                $('.order-list .tab-content div[data-content="' + currenttab + '"]').load(this.href);
-                return false;
-            });
-
-            $(document).ready(function () {
-                $('.order-list .tabs ul li').first().addClass('is-active');
-                var activeTab = $('.order-list .tabs ul li').first().data('tab');
-                currenttab = activeTab;
-                $('.order-list .tab-content div[data-content="' + activeTab + '"]').addClass('is-active');
-                getOrderajax(activeTab);
-                getTotalOrdersAjax();
-            });
-
-            $('.order-list .tabs ul li').on('click', function () {
-                var activeTab = $(this).data('tab');
-                $('.order-list .tabs ul li').removeClass('is-active');
-                $(this).addClass('is-active');
-                currenttab = activeTab;
-                $('.order-list .tab-content div').removeClass('is-active');
-                $('.order-list .tab-content div[data-content="' + activeTab + '"]').addClass('is-active');
-                getOrderajax(activeTab);
-            });
-
-
-
-
-        </script>
+        
 
     </section>
 
