d1cf7c6f7a70ccdeeb834f33907673e840a995cf
ChongJiaMing-lab <1221202126@student.mmu.edu.my>
2024-05-06 08:42:59 +0800

    a

--- fyp-combine/admin/run_query.php
+++ fyp-combine/admin/run_query.php
@@ -39,8 +39,8 @@ if (isset($_POST['order']) || isset($_POST['f1']) || isset($_POST['f2']) || isse
 
         //index1 = date, 0 = month; 2 = year;
         if (isset($from[2]) && isset($from[1]) && isset($from[0])) {
-            $f = $from[1].'-'. $from[0].'-'.$from[2];
-            $f = $f." 00:00:00";
+            $f = $from[1] . '-' . $from[0] . '-' . $from[2];
+            $f = $f . " 00:00:00";
         } else {
             $f = '';
         }
@@ -52,8 +52,8 @@ if (isset($_POST['order']) || isset($_POST['f1']) || isset($_POST['f2']) || isse
         $to = explode("/", $t);
 
         if (isset($to[2]) && isset($to[1]) && isset($to[0])) {
-            $t = $to[1].'-'.$to[0].'-'.$to[2];
-            $t = $t." 23:59:59";
+            $t = $to[1] . '-' . $to[0] . '-' . $to[2];
+            $t = $t . " 23:59:59";
         } else {
             $t = '';
         }
@@ -79,9 +79,9 @@ if (isset($_POST['order']) || isset($_POST['f1']) || isset($_POST['f2']) || isse
     FROM order_ 
     JOIN user_information ON order_.user_id = user_information.ID WHERE 1";
 
-    if(!empty($f))
+    if (!empty($f))
         $query .= " AND time_status >= '$f'";
-    if(!empty($t))
+    if (!empty($t))
         $query .= " AND time_status <= '$t'";
     if (!empty($o))
         $query .= " AND name LIKE '%$o%'";
@@ -122,8 +122,77 @@ if (isset($_POST['order']) || isset($_POST['f1']) || isset($_POST['f2']) || isse
            ' . $row["time_status"] . '
         </td>
         <td>
-            '.  $row_add["address"] . ", " . $row_add["postcode"] . " " . $row_add["city"]
-            . ", " . $row_add["state"].'
+            ' . $row_add["address"] . ", " . $row_add["postcode"] . " " . $row_add["city"]
+            . ", " . $row_add["state"] . '
+        </td>
+        <td>
+            RM' . $row["total_amount"] . '
+        </td>
+        <td>
+            ' . $row["delivery_status"] . '
+        </td>
+    </tr>';
+    }
+    echo $o_output;
+}
+
+if (isset($_POST['from2']) && isset($_POST['to2'])) {
+
+    if (isset($_POST['from2'])) {
+        $f = $_POST['from2'];
+        $from = explode("/", $f);
+
+        if (isset($from[2]) && isset($from[1]) && isset($from[0])) {
+            $f = $from[1] . '-' . $from[0] . '-' . $from[2];
+            $f = $f . " 00:00:00";
+        } else {
+            $f = '';
+        }
+    } else
+        $from = '';
+
+    if (isset($_POST['to2'])) {
+        $t = $_POST['to2'];
+        $to = explode("/", $t);
+
+        if (isset($to[2]) && isset($to[1]) && isset($to[0])) {
+            $t = $to[1] . '-' . $to[0] . '-' . $to[2];
+            $t = $t . " 23:59:59";
+        } else {
+            $t = '';
+        }
+    } else
+        $to = '';
+
+    $query = "SELECT *,user_information.name 
+    FROM order_ 
+    JOIN user_information ON order_.user_id = user_information.ID WHERE 1";
+
+    if (!empty($f))
+        $query .= " AND time_status >= '$f'";
+    if (!empty($t))
+        $query .= " AND time_status <= '$t'";
+
+    $o_run = mysqli_query($connect, $query);
+    $o_output = '';
+
+    while ($row = mysqli_fetch_assoc($o_run)) {
+        $user_id = $row["user_id"];
+        $user = "SELECT * FROM user_information WHERE ID = '$user_id'";
+        $user_run = mysqli_query($connect, $user);
+        $row_user = mysqli_fetch_assoc($user_run);
+
+        $address_id = $row["address_id"];
+        $add = "SELECT * FROM user_address WHERE address_id = '$address_id'";
+        $add_run = mysqli_query($connect, $add);
+        $row_add = mysqli_fetch_assoc($add_run);
+        $o_output .= '<tr onclick="window.location=\'order_detail.php?order_id=' . $row['order_id'] . '\';">
+        <th scope="row">' . $row["order_id"] . '</th>
+        <td>
+            ' . $row["name"] . '<br>
+        </td>
+        <td>
+           ' . $row["time_status"] . '
         </td>
         <td>
             RM' . $row["total_amount"] . '
