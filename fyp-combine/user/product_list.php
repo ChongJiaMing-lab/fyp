b230fefe607c4fd4ee52af0321d0cbb923eb69ae
Lam Wei Leong <1221201835@mmu.edu.my>
2024-06-18 00:44:37 +0800

    fix css

--- fyp-combine/user/product_list.php
+++ fyp-combine/user/product_list.php
@@ -6,6 +6,7 @@
 <html dir="ltr" lang="en-US" class="ready">
 
 <link defer rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
 <head>
 
 
@@ -101,249 +102,255 @@
 <body class="body-style wide  clamp-1">
     <?php include "head.php" ?>
     <style>
-	.btn-wishlist , .btn-compare {
+        .btn-wishlist,
+        .btn-compare {
 
-		display: none;
-	}
-
-	.title {
-    color: black; 
-	
-}
-
-input.button.btn-login{
-            background-color:black;
+            display: none;
         }
 
-a.button.view-password
-{
-            background-color:black;
+        .title {
+            color: black;
+
         }
-.txt-interactt 
-{
-    color: skyblue !important;
-}
-.txt-data, .input.txt-data
-{
-	color:#2586c6;
-}
-.body-style button, .body-style .button 
-{
-    background-color:black;
-}
-	</style>	
+
+        input.button.btn-login {
+            background-color: black;
+        }
+
+        a.button.view-password {
+            background-color: black;
+        }
+
+        .txt-interactt {
+            color: skyblue !important;
+        }
+
+        .txt-data,
+        .input.txt-data {
+            color: #2586c6;
+        }
+
+        .body-style button,
+        .body-style .button {
+            background-color: black;
+        }
+    </style>
     <script defer src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
     <div id="contents">
-					<div id="sidebar-left">
-				<!-- ###AIO### -->
-<div class="holder">
-            <div class="module-container">
+        <div id="sidebar-left">
             <!-- ###AIO### -->
-<style>
-#module-price-sort .box-content {
-			background-color:rgba(255, 255, 255, 1) !important;
-		}
-</style>
-<div id="module-price-sort" class="box utilities">
+            <div class="holder">
+                <div class="module-container">
+                    <!-- ###AIO### -->
+                    <style>
+                        #module-price-sort .box-content {
+                            background-color: rgba(255, 255, 255, 1) !important;
+                        }
+                    </style>
+                    <div id="module-price-sort" class="box utilities">
 
-	<div class="title">
-  		Shop By Price	</div>
-	
-	<div class="box-content">
-        
-		                                        <div class="list-group">
-                                                    
-                                                    <input type="hidden" id="hidden_minimum_price" value="0" />
-                                                    <input type="hidden" id="hidden_maximum_price" value="20000" />
-                                                    <p id="price_show">0 -20000</p>
-                                                    <div id="price_range"></div> 
-                                                    
-                                                    
-                                             
-                                                </div>
-	</div>
-	
-</div>
-        </div>
-            <div class="module-container">
-            <!-- ###AIO### -->
-<style>
-#module-category .box-content {
-			background-color:rgba(255, 255, 255, 1) !important;
-		}
+                        <div class="title">
+                            Shop By Price </div>
 
-        #contents #sidebar-left .module-container .box .box-content, #contents #sidebar-right .module-container .box .box-content, #contents #sidebar-left .module-container .box .box-content a, #contents #sidebar-right .module-container .box .box-content a {
-        font-size: 14px;
-        padding:20px;
-    }
+                        <div class="box-content">
 
-    .list-group
-    {
-        padding: 20px;
-        padding-top: 10px;
-        border: 20px;
-        width: 200px !important;
-    }
-    .list-group-item {
-    padding: 4px 0px !important;
-    font-weight: bold;
-    border: none;
-    width: 150px !important;
-    font-family: "Arial", sans-serif;
-    font-size: 11px;
-    color: #333;
-}
-#label
-{
-    padding-bottom: 1000px !important;
-}
+                            <div class="list-group">
 
-label {
-   
-   display: flex;
-   align-items: center;
-   gap: 10px; 
-}
+                                <input type="hidden" id="hidden_minimum_price" value="0" />
+                                <input type="hidden" id="hidden_maximum_price" value="20000" />
+                                <p id="price_show">0 -20000</p>
+                                <div id="price_range"></div>
 
-.checkbox
-{
-   width: 100px !important;
-   
-}
-.module-product .frame, .tab-product .frame {
-    border-radius: 0px;
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-    vertical-align: bottom;
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-}
-</style>
-<div id="module-category" class="box utilities">
-        <div class="title">Brand</div>
-    <div class="box-content">
-                    <div class="list-group">
-                                        <?php
-                                        $query = "SELECT DISTINCT p.brand_id, b.brand_name 
+
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="module-container">
+                    <!-- ###AIO### -->
+                    <style>
+                        #module-category .box-content {
+                            background-color: rgba(255, 255, 255, 1) !important;
+                        }
+
+                        #contents #sidebar-left .module-container .box .box-content,
+                        #contents #sidebar-right .module-container .box .box-content,
+                        #contents #sidebar-left .module-container .box .box-content a,
+                        #contents #sidebar-right .module-container .box .box-content a {
+                            font-size: 14px;
+                            padding: 20px;
+                        }
+
+                        .list-group {
+                            padding: 20px;
+                            padding-top: 10px;
+                            border: 20px;
+                            width: 200px !important;
+                        }
+
+                        .list-group-item {
+                            padding: 4px 0px !important;
+                            font-weight: bold;
+                            border: none;
+                            width: 150px !important;
+                            font-family: "Arial", sans-serif;
+                            font-size: 13px;
+                            color: #333;
+                        }
+
+                        #label {
+                            padding-bottom: 1000px !important;
+                        }
+
+                        label {
+
+                            display: flex;
+                            align-items: center;
+                            gap: 10px;
+                        }
+
+                        .checkbox {
+                            width: 120px !important;
+
+                        }
+
+                        .module-product .frame,
+                        .tab-product .frame {
+                            border-radius: 0px;
+                            border: none;
+                            background-color: rgba(255, 255, 255, 1);
+                            vertical-align: bottom;
+                            display: grid;
+                            grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+                        }
+                    </style>
+                    <div id="module-category" class="box utilities">
+                        <div class="title">Brand</div>
+                        <div class="box-content">
+                            <div class="list-group">
+                                <?php
+                                $query = "SELECT DISTINCT p.brand_id, b.brand_name 
                 FROM product p
                 INNER JOIN brand b ON p.brand_id = b.brand_id
                 WHERE p.product_status ='1' 
                 ORDER BY p.product_id DESC";
-                                        $statement_brand = $connect->prepare($query); // Use a different variable for statement object
-                                        $execute_result_brand = $statement_brand->execute(); // Execute the statement
-                                        if ($execute_result_brand) {
-                                            $result_brand = $statement_brand->get_result();
-                                            foreach ($result_brand as $row) {
-                                                ?>
-                                                <div class="list-group-item checkbox">
-                                                    <label><input type="checkbox" class="common_selector brand"
-                                                            value="<?php echo $row['brand_id']; ?>">
-                                                        <?php echo $row['brand_name']; ?>
-                                                    </label>
-                                                </div>
-
-                                                <?php
-                                            }
-                                        } else {
-                                            // Handle the case where execute() failed
-                                            echo "Error executing query: " . $statement_brand->error;
-                                        }
+                                $statement_brand = $connect->prepare($query); // Use a different variable for statement object
+                                $execute_result_brand = $statement_brand->execute(); // Execute the statement
+                                if ($execute_result_brand) {
+                                    $result_brand = $statement_brand->get_result();
+                                    foreach ($result_brand as $row) {
                                         ?>
-                                    </div>
-                                    
-    </div>
+                                        <div class="list-group-item checkbox">
+                                            <label><input type="checkbox" class="common_selector brand"
+                                                    value="<?php echo $row['brand_id']; ?>">
+                                                <?php echo $row['brand_name']; ?>
+                                            </label>
+                                        </div>
 
-    <div class="title">Category</div>
-    <div class="box-content">
-                    <div class="list-group">
-                    <?php
-                        $query2 = "SELECT DISTINCT p.category_id, c.category
+                                        <?php
+                                    }
+                                } else {
+                                    // Handle the case where execute() failed
+                                    echo "Error executing query: " . $statement_brand->error;
+                                }
+                                ?>
+                            </div>
+
+                        </div>
+
+                        <div class="title">Category</div>
+                        <div class="box-content">
+                            <div class="list-group">
+                                <?php
+                                $query2 = "SELECT DISTINCT p.category_id, c.category
                                             FROM product p 
                                             INNER JOIN category c ON p.category_id = c.category_id
                                             WHERE p.product_status ='1' 
                                             ORDER BY p.product_id DESC";
 
-                        $statement_category = $connect->prepare($query2); // Use a different variable for statement object
-                        $execute_result_category = $statement_category->execute(); // Execute the statement
-                        if ($execute_result_category) {
-                            $result_category = $statement_category->get_result();
-                            foreach ($result_category as $row) {
-                                ?>
-                                <div class="list-group-item checkbox">
-                                    <label ><input type="checkbox" class="common_selector category"
-                                            value="<?php echo $row['category_id']; ?>">
-                                        <?php echo str_replace("_"," ",$row['category']); ?>
-                                    </label>
-                                </div>
+                                $statement_category = $connect->prepare($query2); // Use a different variable for statement object
+                                $execute_result_category = $statement_category->execute(); // Execute the statement
+                                if ($execute_result_category) {
+                                    $result_category = $statement_category->get_result();
+                                    foreach ($result_category as $row) {
+                                        ?>
+                                        <div class="list-group-item checkbox">
+                                            <label><input type="checkbox" class="common_selector category"
+                                                    value="<?php echo $row['category_id']; ?>">
+                                                <?php echo str_replace("_", " ", $row['category']); ?>
+                                            </label>
+                                        </div>
 
-                                <?php
-                            }
-                        } else {
-                            // Handle the case where execute() failed
-                            echo "Error executing query: " . $statement_category->error;
-                        }
-                        ?>
-                                    </div>
-                                    
-    </div>
-</div>
+                                        <?php
+                                    }
+                                } else {
+                                    // Handle the case where execute() failed
+                                    echo "Error executing query: " . $statement_category->error;
+                                }
+                                ?>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="main-content">
+            <div class="holder">
+                <!-- ###AIO### -->
+
+                <!-- CATEGORY HEADER -->
+                <div class="category-header">
+
+                    <!-- CATEGORY THUMB -->
+                    <div class="category-thumb">
+                        <div class="category-thumb-frame">
+                            <img src="image/techzone/image/data/Untitled design (12).jpg" alt="Digital Memory" />
+                        </div>
+                        <div class="category-thumb-text">
+                            Digital Memory </div>
+                    </div>
+
+                </div>
+
+
+
+
+
+                <!-- PRODUCTS -->
+
+                <!-- PRODUCT FILTER -->
+                <div class="product-filter">
+
+
+
+
+
+                </div>
+                <!-- END PRODUCT FILTER -->
+
+                <!-- PRODUCT LISTING -->
+                <div class="box module-product product-listing ">
+                    <div class="box-content">
+
+
+                    </div>
+                </div>
+
+
+
+
+                <!-- EMPTY PRODUCT -->
+
+                <!-- ###AIO### -->
+            </div>
         </div>
     </div>
- 			</div>
-				<div id="main-content">
-			<div class="holder">
-				<!-- ###AIO### -->
+    </section>
 
-				<!-- CATEGORY HEADER -->
-				<div class="category-header">
-					
-					<!-- CATEGORY THUMB -->
-					<div class="category-thumb">
-						<div class="category-thumb-frame">
-															<img src="image/techzone/image/data/Untitled design (12).jpg" alt="Digital Memory" />
-													</div>
-						<div class="category-thumb-text">
-							Digital Memory						</div>
-					</div>
-					
-									</div>
-				
-
-
-									
-				
-				<!-- PRODUCTS -->
-				
-					<!-- PRODUCT FILTER -->
-					<div class="product-filter">
-
-						
-					
-
-						
-					</div>
-					<!-- END PRODUCT FILTER -->
-
-					<!-- PRODUCT LISTING -->
-					<div class="box module-product product-listing ">
-						<div class="box-content">
-															
-                                				
-						</div>
-					</div>
-
-
-								
-
-				<!-- EMPTY PRODUCT -->
-				
-				<!-- ###AIO### -->
-			</div>
-		</div>
-			</div>
-</section>
-
-<style>
+    <style>
         #loading {
             text-align: center;
             background: url('loading.gif') no-repeat center;
@@ -354,7 +361,7 @@ label {
         $(document).ready(function () {
             filter_data();
             function filter_data() {
-                $('.product-listing .box-content').html('<div id="loading"></div>'); 
+                $('.product-listing .box-content').html('<div id="loading"></div>');
                 var action = 'fetch_data';
                 var minimum_price = $('#hidden_minimum_price').val();
                 var maximum_price = $('#hidden_maximum_price').val();
@@ -385,24 +392,23 @@ label {
             $('.common_selector').click(function () {
                 filter_data();
             });
-            
+
             $('#price_range').slider({
-        range:true,
-        min:0,
-        max:20000,
-        values:[0,20000],
-        step:1,
-        stop:function(event,ui)
-        {
-            $('#price_show').html(ui.values[0]+'-' + ui.values[1]);
-            $('#hidden_minimum_price').val(ui.values[0]);
-            $('#hidden_maximum_price').val(ui.values[1]);
-            filter_data();
-        }
-    });
+                range: true,
+                min: 0,
+                max: 20000,
+                values: [0, 20000],
+                step: 1,
+                stop: function (event, ui) {
+                    $('#price_show').html(ui.values[0] + '-' + ui.values[1]);
+                    $('#hidden_minimum_price').val(ui.values[0]);
+                    $('#hidden_maximum_price').val(ui.values[1]);
+                    filter_data();
+                }
+            });
         });
     </script>
-    
-    </body>
+
+</body>
 
 </html>
