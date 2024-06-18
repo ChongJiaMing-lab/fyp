ece529db1eeb7d88d378f1e7a2d565611f4d831d
ChongJiaMing-lab <1221202126@student.mmu.edu.my>
2024-06-18 19:46:04 +0800

    a

--- fyp-combine/admin/admin_feedback.php
+++ fyp-combine/admin/admin_feedback.php
@@ -1,3 +1,15 @@
 <?php include 'admin_sidebar.php';
 include 'databaseconnect.php';
 ?>
+
+<body>
+    <div class="main p-3">
+        <div class="head" style="display:flex;">
+            <i class="lni lni-comments" style="font-size:50px;"></i>
+            <h1 style="margin: 12px 0 0 30px">Feedback</Category:Components>
+            </h1>
+            <hr>
+        </div>
+    </div>
+
+</body>
